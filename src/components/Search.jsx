90bcf4d202f4b1612031468d31cb05e03cff865b
dnyaneshwarmagar <89983890+dnyaneshwarmagar@users.noreply.github.com>
2022-07-08 15:48:14 +0530

    optimized

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const Search = ({handleSearch}) => {
-    
+const Search = ({ getList }) => {
   return (
     <div>
-    <input type="text" placeholder="Search for a Contact" style={{width:"80vw"}} onChange={(e)=>handleSearch(e.target.value)}/>
-    
-</div>
-  )
-}
+      <input
+        type="text"
+        placeholder="Search for a Contact"
+        style={{ width: "80vw" }}
+        onChange={(e) => getList(e.target.value,1)}
+      />
+    </div>
+  );
+};
 
-export default Search
+export default Search;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,18 +7,21 @@ const Home = () => {
     const [list, setList] = useState([]);
     const [modalShow, setModalShow] = useState(false);
     const [item, setItem] = useState(null);
-    
+    const [page,setPage] = useState(1);
+    const [val,setVal] = useState("")
     useEffect(() => {
-      getList();
+      getList(val,page);
     }, []);
-    const getList = async () => {
+    
+    const getList = async (val,page) => {
       const res = await axios.get(
-        "https://rickandmortyapi.com/api/character/?name=&page=3"
+        `https://rickandmortyapi.com/api/character/?name=${val}&page=${page}`
       );
       console.log(res.data, res.data.results);
+      setVal(val)
       setList(res.data.results);
     };
-    
+
     const handleClick = (item)=>{
       setItem(item)
       setModalShow(true)
@@ -26,17 +29,23 @@ const Home = () => {
     useEffect(()=>{},[modalShow])
     console.log("list", list);
 
-    const handleSearch =async (val)=>{
-      const res = await axios.get(
-        `https://rickandmortyapi.com/api/character/?name=${val}&page=3`
-      );
-      console.log(res.data.results);
-      setList(res.data.results);
-    }
-
+    // useEffect(()=>{ handleInfiniteLoading(val,page)  },[page])
+    // const handleInfiniteLoading =async (val,page)=>{
+    //   const res = await axios.get(
+    //     `https://rickandmortyapi.com/api/character/?name=${val}&page=${page}`
+    //   );
+    //   console.log(res.data.results);
+    //   setList(prev=>[...prev,...res.data.results]);
+    // }
+    // window.onscroll = ()=>{
+    //   if(window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight){
+    //     setPage(page=>page+1)
+      
+    //   }
+    // }
   return (
     <>
-     <Search handleSearch={handleSearch}/>
+     <Search getList={getList} />
      
     {modalShow?<Details show={modalShow} onHide={() => setModalShow(false)} item={item}/>:null}
     <List list={list}  handleClick={handleClick}/>
